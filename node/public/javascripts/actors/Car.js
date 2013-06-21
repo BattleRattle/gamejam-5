1a06c0c224b2fa1c5ad46376ca1137002610e885
Marco Krüger <krgsch@gmail.com>
2013-06-21 23:50:02 +0200

    simple movement

--- node/public/javascripts/actors/Car.js
+++ node/public/javascripts/actors/Car.js
@@ -15,12 +15,12 @@ carActor = gamvas.Actor.extend({
 		this.setFile(st.resource.getImage('images/car.png'));
 
 		// set the actors center point to its lower center
-		//this.setCenter(0,0);
+		//this.setCenter(64,32);
 
         // a car is a moving object
         this.bodyRect(this.position.x, this.position.y, 64, 32, gamvas.physics.DYNAMIC);
 
-        //this.linearDamping(0.15);
+        this.setLinearDamping(0.15);
 
 		// finally add the state to our actor
 		this.addState(new defaultCarActorState('default'));
@@ -29,5 +29,4 @@ carActor = gamvas.Actor.extend({
 		this.setState('default');
 
 	}
-
 });
