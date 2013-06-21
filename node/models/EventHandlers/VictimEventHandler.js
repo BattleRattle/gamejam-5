78b28901c46ebd75d3e1843b1cbb31ea789cfd2b
Woellchen <schmidt.patrick91@gmail.com>
2013-06-21 23:01:07 +0200

    add victim collide handler

--- node/models/EventHandlers/VictimEventHandler.js
+++ node/models/EventHandlers/VictimEventHandler.js
@@ -1,15 +1,33 @@
 var VictimEventHandler = function () {
 
 	this.victimCounter = 0;
+	this.victims = [];
 
 }
 
-VictimEventHandler.prototype.push = function() {
-	return {
+VictimEventHandler.prototype.push = function () {
+	var victim = {
 		'id': ++this.victimCounter,
 		'x': 1130,
 		'y': 230
 	};
+	this.victims.push(victim);
+
+	return victim;
+}
+
+VictimEventHandler.prototype.collide = function (data) {
+	var newVictims = [];
+
+	for (var victim in this.victims) {
+		if (victim.id === data.id) {
+			continue;
+		}
+
+		newVictims.push(victim);
+	}
+
+	this.victims = newVictims;
 }
 
 module.exports = VictimEventHandler;
