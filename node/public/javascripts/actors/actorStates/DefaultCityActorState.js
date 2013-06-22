cb165140f3cbca0d096aca66257b9401b5028ba9
Stephan Vock <stephan.vock@innogames.de>
2013-06-22 05:01:41 +0200

    first auto generated maps (still buggy but too tired to fix atm)

--- node/public/javascripts/actors/actorStates/DefaultCityActorState.js
+++ node/public/javascripts/actors/actorStates/DefaultCityActorState.js
@@ -2,24 +2,29 @@
 defaultCityActorState = gamvas.ActorState.extend({
 	actors: [],
 
+	create: function (name, config) {
+		this._super(name);
+		this.config = config;
+		console.log(config);
+	},
+
 	init: function () {
 		this.dim = gamvas.getCanvasDimension();
 
-		var xTiles = Math.ceil(this.dim.w / 512);
-		var yTiles = Math.ceil(this.dim.h / 512)
-
-		var minX = -xTiles * 512 / 2;
-		var minY = -yTiles * 512 / 2;
+		var minX = -this.config.size.width * 512 / 2;
+		var minY = -this.config.size.height * 512 / 2;
 		var x = minX;
-		for (var xCounter = 0; xCounter < xTiles; xCounter++) {
+		for (var xCounter = 0; xCounter < this.config.size.width; xCounter++) {
 			var y = minY;
-			this.actors[xCounter] = new Array();
-			for (var yCounter = 0; yCounter < yTiles; yCounter++) {
-				this.actors[xCounter][yCounter] = new tileActor("tile" + xCounter + "x" + yCounter, x, y);
+			this.actors[xCounter] = [];
+			for (var yCounter = 0; yCounter < this.config.size.height; yCounter++) {
+				this.actors[xCounter][yCounter] = new tileActor("tile" + xCounter + "x" + yCounter, x, y, this.config.tilesets[this.config.tiles[xCounter][yCounter]]);
 				y += 512;
 			}
 			x += 512;
 		}
+
+		console.log (this.actors)
 	},
 
 	draw: function (t) {
