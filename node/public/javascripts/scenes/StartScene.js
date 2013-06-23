d1f0469e5f377d635d96cbbf5f5019a817395853
norman.soetbeer <contact@rattleserver.de>
2013-06-23 10:49:51 +0200

    pimped startup screen

--- node/public/javascripts/scenes/StartScene.js
+++ node/public/javascripts/scenes/StartScene.js
@@ -4,20 +4,40 @@ startScene = gamvas.State.extend({
 
         // assume our car (96px X 48px) is 4m X 2m
         gamvas.physics.pixelsPerMeter = 24;
+
+		this.logo= new gamvas.Image(this.resource.getImage("/images/startup/logo.png"));
+		this.palms = new gamvas.Image(this.resource.getImage("/images/startup/palm.png"));
+		this.textOpacity = 1.0;
+		this.textOpacityDiff = -1;
 	},
 
+	preDraw: function(t) {
+		var gradient = this.c.createLinearGradient(0, 0, 0, this.dim.h);
+		gradient.addColorStop(0, '#ffbffa');
+		gradient.addColorStop(1, '#ff51dc');
+		this.c.fillStyle = gradient;
+		this.c.fillRect(0, 0, this.dim.w, this.dim.h);
 
-	draw: function(t) {
-		// every state has this.c, which is the 2D context of the canvas
-		// set our draw color to white
-		this.c.fillStyle = '#fff';
-		// pick some nice font
-		this.c.font = 'bold 20px sans-serif';
-		// our text should be drawn centered
+		this.logo.setPosition(this.dim.w / 2 - 672, this.dim.h / 2 - 231);
+		this.logo.draw();
+
+		this.palms.setPosition(this.dim.w - 794, this.dim.h - 621);
+		this.palms.draw();
+
+		this.textOpacity += this.textOpacityDiff * t;
+		if (this.textOpacity >= 1.0) {
+			this.textOpacity = 1.0;
+			this.textOpacityDiff = -1;
+		} else if (this.textOpacity < 0.2) {
+			this.textOpacity = 0.2;
+			this.textOpacityDiff = 1;
+		}
+
+		this.c.fillStyle = 'rgba(255,255,255,' + this.textOpacity + ')';
+		this.c.font = 'bold 30px sans-serif';
 		this.c.textAlign = 'center';
-		// draw the text (note that every state has a default
-		// camera that points to position 0/0)
-		this.c.fillText("Violence suxx but we are awesome!", 0, 0);
+		this.c.fillText("PRESS ANY KEY TO START THE GAME!", this.dim.w / 2, this.dim.h - 100);
+//		this.c.fillText("Violence suxx but we are awesome!", 0, 0);
 	},
 
 	postDraw: function(t) {
